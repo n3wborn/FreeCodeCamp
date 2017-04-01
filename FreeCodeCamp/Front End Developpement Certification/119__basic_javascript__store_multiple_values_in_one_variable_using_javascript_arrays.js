6f5c00b0c5c6925e55d0d48a1d19d1d5f1939690
n3wborn <stefouy@gmail.com>
2017-04-01 13:37:06 +0200

    add files - 119 to 124

---
+++ FreeCodeCamp/Front End Developpement Certification/119__basic_javascript__store_multiple_values_in_one_variable_using_javascript_arrays.js
@@ -0,0 +1,20 @@
+/*
+ * With JavaScript array variables, we can store several pieces of data in
+ * one place.
+ *
+ * You start an array declaration with an opening square bracket, end it with
+ * a closing square bracket, and put a comma between each entry, like this:
+ *
+ *		var sandwich = ["peanut butter", "jelly", "bread"].
+ *
+ * Instructions
+ *
+ * Modify the new array myArray so that it contains both a string and a
+ * number (in that order).
+ *
+ * Hint
+ * Refer to the example code in the text editor if you get stuck.
+ */
+
+var myArray = ["Answer", 42];
+
---
+++ FreeCodeCamp/Front End Developpement Certification/120__basic_javascript__nest_one_array_within_another_array.js
@@ -0,0 +1,12 @@
+/*
+ * You can also nest arrays within other arrays, like this:
+ * ["Bulls", 23], ["White Sox", 45]]
+ * This is also called a Multi-dimensional Array.
+ *
+ * Instructions
+ *
+ * Create a nested array called myArray.]
+ */
+
+var myArray = ["array1", ["array2", 33]];
+
