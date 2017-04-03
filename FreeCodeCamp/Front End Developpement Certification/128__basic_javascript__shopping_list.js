a361ae228163490d1c2e1bfcb44826c6fb69bb12
n3wborn <stefouy@gmail.com>
2017-04-03 12:33:35 +0200

    add files - 125 to 131

---
+++ FreeCodeCamp/Front End Developpement Certification/128__basic_javascript__shopping_list.js
@@ -0,0 +1,16 @@
+/*
+ * Create a shopping list in the variable myList. The list should be a
+ * multi-dimensional array containing several sub-arrays.
+ *
+ * The first element in each sub-array should contain a string with the name
+ * of the item. The second element should be a number representing the
+ * quantity i.e.
+ *
+ * ["Chocolate Bar", 15]
+ *
+ * There should be at least 5 sub-arrays in the list.
+ */
+
+var myList = [["farine", 1], ["sucre", 1], ["oeufs", 3], ["lait", 2], \
+				["vanille", 2]];
+
