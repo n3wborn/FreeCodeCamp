8f26ab622fae0449dfcdcab55c5b32055f4da0a2
n3wborn <stefouy@gmail.com>
2017-04-03 23:37:28 +0200

    add files - 132 to 144

---
+++ FreeCodeCamp/Front End Developpement Certification/134__basic_javascript__return_a_value_from_a_function_with_return.js
@@ -0,0 +1,30 @@
+/*
+ * We can pass values into a function with arguments. You can use a return
+ * statement to send a value back out of a function.
+ *
+ * Example
+ *
+ *		function plusThree(num) {
+ *			return num + 3;
+ *		}
+ *
+ *		var answer = plusThree(5); // 8
+ *
+ * plusThree takes an argument for num and returns a value equal to num + 3.
+ *
+ * Instructions
+ *
+ * Create a function timesFive that accepts one argument, multiplies it by 5,
+ * and returns the new value.
+ */
+
+// Example
+function minusSeven(num) {
+  return num - 7;
+}
+
+// Only change code below this line
+function timesFive(arg) {
+  return arg * 5;
+}
+
