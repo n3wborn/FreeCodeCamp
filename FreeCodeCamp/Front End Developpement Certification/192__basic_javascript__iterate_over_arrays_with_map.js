2f1b8e4127d0af7aefff9e22d349cd35b117faab
n3wborn <stefouy@gmail.com>
2017-04-26 23:29:40 +0200

    add files - 192 to 194

--- FreeCodeCamp/Front End Developpement Certification/192__basic_javascript__iterate_over_arrays_with_map.js
+++ FreeCodeCamp/Front End Developpement Certification/192__basic_javascript__iterate_over_arrays_with_map.js
@@ -21,3 +21,11 @@
  * save the results into variable newArray. oldArray should not change.
  */
 
+var oldArray = [1,2,3,4,5];
+
+// Only change code below this line.
+var newArray = oldArray;
+var newArray = oldArray.map(function(val){
+	return val + 3;
+});
+
