58b94d02d5a588714773d3d7c9378a6783c4372f
n3wborn <stefouy@gmail.com>
2017-06-06 17:57:12 +0200

    add file - 1_use_the_JavaScript_console.js

---
+++ FreeCodeCamp/Back End Developpement Certification/Automated Testing and Debugging/1_use_the_JavaScript_console.js
@@ -0,0 +1,16 @@
+/*
+ * Both Chrome and Firefox have excellent JavaScript consoles, also known as
+ * DevTools, for debugging your JavaScript.
+ *
+ * You can find Developer tools in your Chrome's menu or Web Console in
+ * FireFox's menu. If you're using a different browser, or a mobile phone, we
+ * strongly recommend switching to desktop Firefox or Chrome.
+ *
+ * Let's print to this console using the console.log method.
+ *
+ *  console.log('Hello world!');
+ *
+ */
+
+console.log('Hello world!');
+
