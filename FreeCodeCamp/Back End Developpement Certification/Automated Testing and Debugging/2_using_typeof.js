469c0a325c2eebe65014afc7a76e645274f98861
n3wborn <stefouy@gmail.com>
2017-06-06 19:22:53 +0200

    add file - 2_using_typeof.js

---
+++ FreeCodeCamp/Back End Developpement Certification/Automated Testing and Debugging/2_using_typeof.js
@@ -0,0 +1,18 @@
+/*
+ * You can use typeof to check the data structure, or type, of a variable.
+ *
+ * Note that in JavaScript, arrays are technically a type of object.
+ *
+ * Try using typeof on each of the following to see which types they have.
+ *
+ *  console.log(typeof "");
+ *  console.log(typeof 0);
+ *  console.log(typeof []);
+ *  console.log(typeof {});
+ */
+
+console.log(typeof "");
+console.log(typeof 0);
+console.log(typeof []);
+console.log(typeof {});
+
