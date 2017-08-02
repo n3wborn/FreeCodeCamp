03f180f4dff280a81bf6775059b026d8f6a55f2c
stef <stef@localhost.localdomain>
2017-08-02 16:47:44 +0200

    solve 230__Intermediate_Algorithm__missing_letters.js (help needed for this one :/ )

--- FreeCodeCamp/Front End Developpement Certification/230__Intermediate_Algorithm__missing_letters.js
+++ FreeCodeCamp/Front End Developpement Certification/230__Intermediate_Algorithm__missing_letters.js
@@ -11,36 +11,20 @@
  */
 
 function fearNotLetter(str) {
-	var prev = str [0];
-	var itented = "";
-	var next = prev + 1;
-	for (var i = 0; i < str.length; i++) {
-		// compare str[n] and str[n + 1] and check if the next is the one
-		// it must be in alphabetically order.
-		// NOTE:
-		//	str.charCodeAt(index) -> Need an index -> return integer
-		//	String.fromCharCode(65, 66, 67) -> Need a unicode -> return char (ABC)
-		//
-		// DEBUG
-		console.log("i ->", i);
-		// prochain en fonction de l'alphabet -> String.fromCharCode(str.charCodeAt(i + 1))
-		// prochain char -> str.charCodeAt(i + 1)
-		// FIXME!
-		//	 ↓
-		console.log("String.fromCharCode(str.charCodeAt(i + 1)) ->", String.fromCharCode(str.charCodeAt(i + 1)));
-		console.log("str.charCodeAt(i)",str.charCodeAt(i));
-		console.log("str.charCodeAt(i + 1)",str.charCodeAt(i + 1));
-		if (String.fromCharCode(str.charCodeAt(i + 1)) !== str.charCodeAt(i + 1)) {
-			return String.fromCharCode(str.charCodeAt(i + 1));
-		} else {
-			return undefined;
-		}
-	}
+
+    for(var i = 0; i < str.length; i++) {
+        var charCode = str.charCodeAt(i);
+
+        if (charCode !== str.charCodeAt(0) + i) {
+            return String.fromCharCode(charCode - 1);
+        }
+    }
+    return undefined;
 }
 
 // tests
 fearNotLetter("abce");				// "d"
-//fearNotLetter("abcdefghjklmno");	// "i"
-//fearNotLetter("bcd");				// undefined
-//fearNotLetter("yz");				// undefined
+fearNotLetter("abcdefghjklmno");	// "i"
+fearNotLetter("bcd");				// undefined
+fearNotLetter("yz");				// undefined
 
